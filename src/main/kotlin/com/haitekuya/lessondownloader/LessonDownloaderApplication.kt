3b9414b835195eed07f6a998997705fed97ce909
Tadashi Yamasaki <yamasaki@haitekuya.com>
2019-01-23 23:49:28 +0900

    Initial commit

---
+++ src/main/kotlin/com/haitekuya/lessondownloader/LessonDownloaderApplication.kt
@@ -0,0 +1,12 @@
+package com.haitekuya.lessondownloader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LessonDownloaderApplication
+
+fun main(args: Array<String>) {
+    runApplication<LessonDownloaderApplication>(*args)
+}
+
